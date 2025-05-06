c0572a5b7a79dbbffb5c92b01e9662465d229b6b
ELMilkMan <730254@student.dusd.net>
2025-05-06 11:45:17 -0700

    all is left is harsd mode and GDD
    >;3

--- P4U3Ruvalcaba/Assets/Scripts/PlayerController.cs
+++ P4U3Ruvalcaba/Assets/Scripts/PlayerController.cs
@@ -9,14 +9,20 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce = 2.0f;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
     public AudioClip crashSound;
     public AudioClip jumpSound;
+    private AudioSource playerAudio;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
@@ -27,12 +33,23 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)&& isOnGround && !gameOver)
         {
+            playerAudio.PlayOneShot(jumpSound, 2.0f);
+
             dirtParticle.Stop();
 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
-        } 
+
+            doubleJumpUsed = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&& !isOnGround&&!doubleJumpUsed)
+        {
+            doubleJumpUsed= true;
+            playerRb.AddForce(Vector3.up*doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3,0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -46,10 +63,14 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 4.0f);
+
         }
         else if (collision.gameObject.CompareTag("Ground"))
         {
             dirtParticle.Play();
+
+            
 
             isOnGround=true;
             isOnGround=true;
--- P4U3Ruvalcaba/Assets/Scripts/SpawnManger.cs
+++ P4U3Ruvalcaba/Assets/Scripts/SpawnManger.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class SpawnManger : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController PlayerControllerScript;
-    
+    private int randomObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +28,13 @@ public class SpawnManger : MonoBehaviour
 
 
     void SpawnObstacle()
+        
     {
+        randomObstacle = Random.Range(0, obstaclePrefabs.Length);
         if (PlayerControllerScript.gameOver == false)
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
     }
+
 }
+
 
